fa13ad3c4d8060f07b837ae1c2151063d92b367c
Sayantan Bose <75779600+Sayantan19@users.noreply.github.com>
2023-01-14 05:08:17 +0530

    edited the css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,13 +8,13 @@ export default function Contact() {
                     <h2 className="text-uppercase heading text">Contact Me</h2>
                     <div className="row">
                         <div className="col">
-                            <ul>
+                            <ul className='list-unstyled'>
                                 <li>Email: <a href="mailto:sayantan485@gmail.com"  id="con">sayantan485@gmail.com</a></li>
                                 <li>Phone/WhatsApp: <a href="tel:+919477517114"  id="con">9477517114</a></li>
                             </ul>
                         </div>
                         <div className="col">
-                            <ul>
+                            <ul className='list-unstyled'>
                                 <li>LinkedIn: <a href="https://www.linkedin.com/in/sayantan-bose-860791200/"  id="con">https://www.linkedin.com/in/sayantan-bose-860791200/</a></li>
                                 <li>Instagram: <a href="https://www.instagram.com/shaa_yon_ton/"  id="con">https://www.instagram.com/shaa_yon_ton/</a></li>
                             </ul>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import './Footer.css'
 export default function Footer() {
     return (
         <section>
-            <footer class="text-center bg-black">
-                <div class="container text-white py-4 py-lg-5">
-                    <p class="text-muted mb-0">Copyright © {new Date().getFullYear()} Sayantan Bose</p>
+            <footer className="text-center bg-black">
+                <div className="container text-white py-4 py-lg-5">
+                    <p className="text-muted mb-0">Copyright © {new Date().getFullYear()} Sayantan Bose</p>
                 </div>
             <a className='btn btn-outline-light mb-4' role="button" id="button1" href="/#bgcomp">Back to top</a>
             </footer>
